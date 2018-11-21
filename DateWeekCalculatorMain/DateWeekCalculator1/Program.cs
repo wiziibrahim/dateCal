116d9d757228ab30dcd01a12c65b0556efa45591
Ibrahim <acheneje.ibrahim@yahoo.co.uk>
2018-11-21 21:40:59 +0000

    datecalc

---
+++ DateWeekCalculatorMain/DateWeekCalculator1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateWeekCalculator1
+{
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            LoginData loginID = new LoginData();//login class LoginData
+            Console.WriteLine("\n\n\n");
+            Console.Write("\t\t\t");
+            Console.WriteLine("Enter username:");
+            Console.Write("\t\t\t");
+            String Username = Console.ReadLine();
+            Console.Write("\t\t\t");
+            Console.WriteLine("Enter Password:");
+            Console.Write("\t\t\t");
+            String Password = Console.ReadLine();
+
+            String account = LoginData.loginInfo(Username, Password);
+            if (account=="locked")
+            {
+                //Console.ReadKey();
+                return;
+            }
+            else
+            {
+                Console.Write("\t\t\t");
+                Console.WriteLine(account);
+            }
+
+            string months = "January February March April May June July August September October November December";
+        var splitMonths = months.Split(' ');//split the string months into an array by whitespace ' '
+                                            /*Console.WriteLine(splitMonths[0]);//return string in position 0 which is January
+                                            Console.WriteLine(splitMonths[0].Length);//prints to console length of January
+                                            Console.WriteLine(months.Length);//prints to console length of string month
+                                            var whiteSpace = months.IndexOf(' ');//evaluate the position of the first whitespace in string months
+                                            //Console.WriteLine(whiteSpace);// print the position represented by the first whitespace in months
+                                            var L = months.LastIndexOf(Console.ReadLine());
+                                            Console.WriteLine(L);*/
+
+        Console.WriteLine("\n\n");//create new line in console
+        Console.Write("\t\t\t");//create tab in console
+        Console.WriteLine("Enter the Month of the year:");
+        Console.Write("\t\t\t");
+        string newMonth = Console.ReadLine();//enter month of the year on Console and store in newMonth
+        int ii;
+        for (ii = 0; ii < 12; ii++)
+        {
+            //Console.WriteLine(splitMonths[ii]);
+            if (newMonth == splitMonths[ii])//if  newMonth is equal to the matching month in the for loop
+                                            //execute the code below. The purpose of this code is to determine the number of the particular month
+                                            //in the year. If April was entered on the console then ii+1 should be 4.
+            {
+                Console.Write("\t\t\t");
+                Console.WriteLine("Month Entered is - " + splitMonths[ii]);
+                Console.Write("\t\t\t");
+                Console.WriteLine("Month " + (ii + 1) + " of the year");
+
+                Console.ReadKey();
+                Console.WriteLine("\n\n");
+                Console.Write("\t\t\t");
+                Console.WriteLine("Months to Year End as Follows:");
+                Console.WriteLine("\n");
+                int startMonthCal;
+                for (startMonthCal = ii + 1; startMonthCal < 12; startMonthCal++)//display the remaining months of the year                                                                                //starting from month determined in for loop above. 
+                {
+                    Console.Write("\t\t\t");
+                    Console.WriteLine(splitMonths[startMonthCal]);
+                }
+                //Console.ReadKey();
+                // return;//at end of result return to start of code. 
+                Console.WriteLine("\n");
+                Console.Write("\t\t\t");
+                Console.WriteLine("Enter date of the month");
+                Console.Write("\t\t\t");
+                int enterDateData = int.Parse(Console.ReadLine()); //Convert number string from console to integer
+                    weeksCalculation33 getDate44 = new weeksCalculation33();
+                string showDateResult = weeksCalculation33.getDate(enterDateData, splitMonths[ii]); //execute method getDate with two parameters (date and month)               
+                string[] NewDate = showDateResult.Split(' ');
+                    Console.Write("\t\t\t");
+                    Console.WriteLine("Date Entered is - " + NewDate[0] + " " + NewDate[1]);
+                    Console.Write("\t\t\t");
+                    Console.WriteLine("Days to end of " + NewDate[1] + " = " + NewDate[2]);
+                int UU = int.Parse(NewDate[2]);
+                    //Console.WriteLine(UU);
+                    Console.WriteLine("\n\t\t\t");
+                    Console.WriteLine("\n\t\t\tEnter year: ");
+                    Console.Write("\t\t\t");
+                    int year = int.Parse(Console.ReadLine());
+                    Console.WriteLine("\n\n\n");
+                    Console.Write("\t\t\t");
+                    Console.WriteLine("Enter number of weeks");
+                    Console.Write("\t\t\t");
+                    int weekstoDays = int.Parse(Console.ReadLine()) * 7;// convert weeks into integer and get total no of days
+
+                    Console.Write("\t\t\t");
+                    Console.WriteLine("Number of weeks = " + weekstoDays + "days");
+                    //Console.WriteLine("date - " + enterDateData);
+
+                    string result = returnDatefromWeeks.returnDate(enterDateData, weekstoDays, newMonth, year);
+                    Console.WriteLine("\n\n\t\t\t" + (weekstoDays/7) + 
+                        " weeks from" + " " + NewDate[0] + " " + NewDate[1] + " " + year + " is:-" + "\n\t\t\t"+ result);                  
+                    Console.ReadKey();
+            }
+        }
+    }    
+}
+}
---
+++ DateWeekCalculatorMain/DateWeekCalculator1/returnDatefromWeeks.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateWeekCalculator1
+{
+    class returnDatefromWeeks
+    {
+        public static string returnDate(int day, int noOfweeksDays, string month, int year)
+        {
+            string monthCheck4 = "January March May July August October December";//months containing 31 days
+            var monthCheck5 = "April June September November Null Null Null";//months containing 30 days
+            string[] monthCheckSplitWhSpace11 = monthCheck4.Split(' ');//converts string monthCheck to an array
+            var monthCheckSplitWhsSpace12 = monthCheck5.Split(' ');
+            string monthCheck6 = "February Null Null Null Null Null Null";//month with 28 days
+            var monthCheckSplitWhSpace13 = monthCheck6.Split(' ');
+            string JanuaryToDecember = "January February March April May June July August September October November December";
+
+            int i;
+            int k;
+            int m;
+            int countDays = day;
+            int countDownWeeks = noOfweeksDays;
+
+            for (i = 0; i < 7; i++)
+            {
+                //Console.WriteLine("main for loop count - " + i);
+                if (month == monthCheckSplitWhSpace11[i])//check if month entered on console matches a month with 31 days
+                {
+                    Console.WriteLine("\n\n\t\t\t31 days month");
+                    for (k = 0; k <= 4; k++)//for loop for week count
+                    {
+                        countDays = countDays + 7;//add 7 days to current day count to get day of the following week 
+                        Console.Write("\t\t\t");
+                        Console.WriteLine("day - " + countDays + ", month - " + monthCheckSplitWhSpace11[i]);
+                        countDownWeeks = countDownWeeks - 7;//decrement week count by 7
+                        //Console.WriteLine("\t\t\tcheck countDownWeeks = " + countDownWeeks);
+                        //Console.WriteLine("\t\t\tK count = " + k + "\n");
+                        if (countDownWeeks == 0)//check if week count is zero to return final date of the month
+                        {
+                            if (countDays > 31)//check if the current day count goes into the next month
+                            {
+                                countDays = countDays - 31;//obtain the day count in the following month
+                                string[] nextMonth = JanuaryToDecember.Split(' ');//create an array of months from Jan to Dec
+                                for (m = 0; m <= 11; m++)
+                                {
+                                    if (monthCheckSplitWhSpace11[i] == nextMonth[m])//search for the current month
+                                    {
+                                        if (monthCheckSplitWhSpace11[i] == "December")//if condition is true set month to January 
+                                        {
+                                            month = "January"; //set month to January 
+                                            year++;//increment the year by 1 since end of 2018 is reached - beyond 31 Dec of current year
+                                            //Console.WriteLine("start of new year");
+                                            m = 11;
+                                            k = 4;
+                                            i = -1;
+                                        }
+                                        else
+                                        {
+                                            month = nextMonth[m + 1];//obtain the next month 
+                                            m = 11;//make m = 11 to exit the for (m = 0; m < 11; m++) loop
+                                            k = 4;
+                                            i = -1;
+                                        }
+                                    }
+                                }
+                                return countDays + " " + month + " " + year;//return days count of next month if weeks count equals zero and days count>31
+                            }
+                            else// else if week count is not equal to zero
+                            {
+                                return countDays + " " + month + " " + year;
+                                //return countDays + " " + monthCheckSplitWhSpace11[i];//return days count and current month if week count equals zero but the days is less than 31
+                            }//the return statement above ends the for loop returns the value to the main program, as such codes below are not executed as a result.
+                        }//if the "if statement" is not true nothing is returned and the codes below are executed.
+
+                        if (countDays > 31)//check if days count is greater than 31 - if condition is not true skid the block
+                        {
+                            countDays = countDays - 31;//if above statement is true obtain days count of next month
+                            string[] nextMonth = JanuaryToDecember.Split(' ');//create an array of months from Jan to Dec
+                            for (m = 0; m <= 11; m++)
+                            {
+                                if (monthCheckSplitWhSpace11[i] == nextMonth[m])//search for a match of the current month from Jan to Dec
+                                {
+                                    if(monthCheckSplitWhSpace11[i] == "December")
+                                    {
+                                        month = "January";
+                                        year++;
+                                        //Console.WriteLine("start of new year");
+                                        m = 11;
+                                        k = 4;
+                                        i = -1;
+                                    }
+                                    else
+                                    {
+                                        month = nextMonth[m + 1];//obtain the next month
+                                        m = 11;//make m 11 to exit the for (m = 0; m < 11; m++) for loop
+                                        Console.Write("\t\t\t");
+                                        Console.WriteLine("\n\t\t\tEnd of the month reached\n" + "\t\t\tDay of next month - " + countDays + "\n" + "\t\t\tNext month - " + month + "\n");
+                                        k = 4;//make k=4 to end the for loop as the end of the current month is reached.
+                                        i = -1;//setting -=-1 allows the for (i = 0; i < 7; i++) to start count from 0.
+                                    }                                   
+                                }
+                            }
+                           // Console.WriteLine("End of the month reached,  day - " + countDays + "  month - " + month);
+                            //Console.WriteLine("Week count - " + countDownWeeks + ",   k count = " + k);
+                        }
+                    }
+                }
+                
+
+                else if (month == monthCheckSplitWhsSpace12[i])//if month matches a month with 30 days
+                {
+                    Console.WriteLine("\n\t\t\t30 days month");
+                    for (k = 0; k <= 4; k++)
+                        {
+                            countDays = countDays + 7;
+                            countDownWeeks = countDownWeeks - 7;
+                            Console.Write("\t\t\t");
+                            Console.WriteLine("day - " + countDays + ", month - " + monthCheckSplitWhsSpace12[i]);
+                        if (countDownWeeks == 0)
+                        {
+                                if (countDays > 30)
+                                {
+                                    countDays = countDays - 30;
+                                    string[] nextMonth = JanuaryToDecember.Split(' ');
+                                    for (m = 0; m < 11; m++)
+                                    {
+                                        if (month == nextMonth[m])
+                                        {
+                                            month = nextMonth[m + 1];
+                                            m = 11;
+                                            Console.Write("\t\t\t");
+                                            Console.WriteLine("\n\t\t\tEnd of the month reached\n" + "\t\t\tDay of next month - " + countDays + "\n" + "\t\t\tNext month - " + month + "\n");
+                                            k = 4;
+                                            i = -1;
+                                        }
+                                    }
+                                    return countDays + " " + month + " " + year;
+                                }
+                                else
+                                {
+                                    return countDays + " " + month + " " + year;
+                                }
+                        }
+                            if (countDays > 30)
+                            {
+                                countDays = countDays - 30;
+                                string[] nextMonth = JanuaryToDecember.Split(' ');
+                                for (m = 0; m < 11; m++)
+                                {
+                                    if (month == nextMonth[m])
+                                    {
+                                        month = nextMonth[m + 1];
+                                        m = 11;
+                                        Console.Write("\t\t\t");
+                                        Console.WriteLine("\n\t\t\tEnd of the month reached\n" + "\t\t\tDay of next month - " + countDays + "\n" + "\t\t\tNext month - " + month + "\n");
+                                        k = 4;
+                                        i = -1;
+                                    }
+                                }
+                            }
+                    }
+                }
+                else if (month == monthCheckSplitWhSpace13[i])//if month matches a month with 28 days
+                {
+                    //Console.WriteLine("end of the month second check,  day - " + countDays + "  month - " + month);
+                    //int n = (28 - countDays) / 7;
+                    Console.WriteLine("\n\t\t\t28 days month");
+                    for (k = 0; k <= 4; k++)
+                    {
+                        Console.Write("\t\t\t");
+                        Console.WriteLine("day - " + countDays + ", month - " + monthCheckSplitWhSpace13[i]);
+                        countDays = countDays + 7;
+                        countDownWeeks = countDownWeeks - 7;
+                        //Console.WriteLine("final result,  "+ "day  -"+ countDays + "weeks count - " + countDownWeeks);
+                        if (countDownWeeks == 0)
+                        {
+                                    if (countDays > 28)
+                                    {
+                                        countDays = countDays - 28;
+                                        string[] nextMonth2 = JanuaryToDecember.Split(' ');
+                                        Console.WriteLine("final result,  " + countDays + "month - " + month);
+                                        for (m = 0; m < 11; m++)
+                                        {
+                                            if (monthCheckSplitWhSpace13[i] == nextMonth2[m])
+                                            {
+                                                month = nextMonth2[m + 1];
+                                                Console.Write("\t\t\t");
+                                                Console.WriteLine("\n\t\t\tEnd of the month reached\n" + "\t\t\tDay of next month - " + countDays + "\n" + "\t\t\tNext month - " + month + "\n");
+                                                m = 11;
+                                                k = 4;
+                                                i = -1;
+                                            }
+                                        }                               
+                                        return countDays + " " + month + " " + year;
+                                    }
+                                    else
+                                    {
+                                        return countDays + " " + monthCheckSplitWhSpace13[i] + "  " + year;
+                                    }
+                        }
+
+                                if (countDays > 28)
+                                {
+                                    countDays = countDays - 28;
+                                    string[] nextMonth3 = JanuaryToDecember.Split(' ');
+                                    for (m = 0; m < 11; m++)
+                                    {
+                                        if (monthCheckSplitWhSpace13[i] == nextMonth3[m])
+                                        {
+                                            month = nextMonth3[m + 1];
+                                            Console.Write("\t\t\t");
+                                            Console.WriteLine("\n\t\t\tEnd of the month reached\n" + "\t\t\tDay of next month - " + countDays + "\n" + "\t\t\tNext month - " + month + "\n");
+                                            m = 11;                                           
+                                            k = 4;
+                                            i = -1;
+                                        }
+                                    }
+
+                                }
+                    }
+                }                
+            }
+            return "Invalid data entered";
+        }
+    }
+}
